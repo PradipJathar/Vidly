a8faeef2979fc44030686a815b2c3939f53191ce
PradipJathar <jatharpradipofficial@gmail.com>
2023-08-25 11:38:34 +0530

    06 Building RESTful Services with ASP.NET Web API

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,8 +1,11 @@
-﻿using IdentitySample.Models;
+﻿using AutoMapper;
+using IdentitySample.Models;
 using System.Data.Entity;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Vidly.App_Start;
 
 namespace IdentitySample
 {
@@ -12,6 +15,8 @@ namespace IdentitySample
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(Vidly.WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
